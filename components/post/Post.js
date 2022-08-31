4aec5da36a166422b9bde33b0e0c12838f640d97
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-08-31 15:46:37 -0400

    Created reusable card ui component for posts

---
+++ components/post/Post.js
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+import Card from '../ui/Card';
+
+const Post = props => {
+  return (
+    <Fragment>
+      <Card></Card>
+    </Fragment>
+  );
+};
+
+export default Post;
