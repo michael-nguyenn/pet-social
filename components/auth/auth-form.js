9813cda6d5c3ba9124d6839686ac5c9d1224066a
Michael Nguyen <n93michael@gmail.com>
2022-08-30 21:55:29 -0400

    tablet styling done for hero and auth form

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -33,45 +33,49 @@ function AuthForm() {
 
   return (
     <section className={classes.auth}>
-      <h1 className={classes.auth__header}>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form className={classes.form} onSubmit={submitHandler}>
-        <div className={classes.form__container}>
-          <label htmlFor="email" className={classes.form__label} />
-          <input
-            type="email"
-            id="email"
-            placeholder="Email"
-            required
-            ref={emailInputRef}
-            className={classes.form__input}
-          />
-        </div>
+      <div className={classes.auth__wrapper}>
+        <h1 className={classes.auth__header}>
+          {isLogin ? 'Login' : 'Sign Up'}
+        </h1>
+        <form className={classes.form} onSubmit={submitHandler}>
+          <div className={classes.form__container}>
+            <label htmlFor="email" className={classes.form__label} />
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              required
+              ref={emailInputRef}
+              className={classes.form__input}
+            />
+          </div>
 
-        <div className={classes.form__container}>
-          <label htmlFor="password" className={classes.form__label} />
-          <input
-            type="password"
-            id="password"
-            placeholder="Password"
-            required
-            ref={passwordInputRef}
-            className={classes.form__input}
-          />
-        </div>
+          <div className={classes.form__container}>
+            <label htmlFor="password" className={classes.form__label} />
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              required
+              ref={passwordInputRef}
+              className={classes.form__input}
+            />
+          </div>
 
-        <div className={classes.button}>
-          <button className={classes.button__action}>
-            {isLogin ? 'Login' : 'Create Account'}
-          </button>
-          <button
-            type="button"
-            className={classes.button__toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'New to Pet Social?' : 'Already have an account?'}
-          </button>
-        </div>
-      </form>
+          <div className={classes.button}>
+            <button className={classes.button__action}>
+              {isLogin ? 'Login' : 'Create Account'}
+            </button>
+            <button
+              type="button"
+              className={classes.button__toggle}
+              onClick={switchAuthModeHandler}
+            >
+              {isLogin ? 'New to Pet Social?' : 'Already have an account?'}
+            </button>
+          </div>
+        </form>
+      </div>
     </section>
   );
 }
