a79ca89fdd71db5244f1844381e8b3c8078695b6
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-09-01 10:40:28 -0400

    Added mobile styles to post component

--- components/post/Post.js
+++ components/post/Post.js
@@ -2,53 +2,69 @@ import classes from './Post.module.scss';
 import dummy from '../../assets/images/dummy_post.jpeg';
 import heart from '../../assets/icons/heart-icon.png';
 import comment from '../../assets/icons/comment-icon.jpeg';
+import user from '../../assets/icons/account-icon.png';
 import Image from 'next/image';
-import { Fragment } from 'react';
 import Card from '../ui/Card';
 
 const Post = props => {
   return (
-    <Fragment>
-      <Card>
-        <div className={classes.user}>
-          <Image className={classes.user__icon} alt="User icon"></Image>
-          <h4 className={classes.user__name}>Chilli the Chinchilla</h4>
-        </div>
-        <div className={classes.post}>
+    <Card>
+      <div className={classes.user}>
+        <Image
+          className={classes.user__icon}
+          src={user}
+          alt="User icon"
+        ></Image>
+        <p className={classes.user__name}>Chilli the Chinchilla</p>
+      </div>
+      <div className={classes.post}>
+        <Image
+          className={classes.post__image}
+          src={dummy}
+          alt="Posted image"
+          layout="intrinsic"
+        ></Image>
+      </div>
+      <div className={classes.postActivity}>
+        <div className={classes.actionIcons}>
           <Image
-            className={classes.post__image}
-            src={dummy}
-            alt="Posted image"
+            className={classes.actionIcons__likeImg}
+            src={heart}
+            alt="Like icon"
+          ></Image>
+          <Image
+            className={classes.actionIcons__commentImg}
+            src={comment}
+            alt="Comment icon"
+            height={40}
+            width={40}
           ></Image>
         </div>
-        <div className={classes.actionIcons}>
-          <button className={classes.actionIcons__like}>
-            <Image
-              className={classes.actionIcons__likeImg}
-              src={heart}
-              alt="Like icon"
-            ></Image>
-          </button>
-          <button className={classes.actionIcons__comment}>
-            <Image
-              className={classes.actionIcons__commentImg}
-              src={comment}
-              alt="Comment icon"
-            ></Image>
-          </button>
+        <div className={classes.caption}>
+          <span className={classes.caption__user}>Chilli the Chinchilla</span>
+          <span className={classes.caption__text}>
+            Love a good dust bath! I hope mom doesn&apos;t give me a real bath!
+          </span>
+          <p></p>
+        </div>
+        <div className={classes.commentsCollapsed}>
+          <span>View all 24 comments</span>
         </div>
         {/* map out comments here */}
         <div className={classes.comments}>
-          <p className={classes.comments_commenter}></p>
-          <p className={classes.comments_comment}></p>
+          <span className={classes.comments__commenter}>Dot the Doodle</span>
+          <span className={classes.comments__comment}>You look marvelous!</span>
         </div>
         <form className={classes.commentForm}>
-          <input className={classes.commentForm__input}></input>
+          <input
+            className={classes.commentForm__input}
+            placeholder="Add a comment..."
+          ></input>
           <button className={classes.commentForm__button}>Post</button>
         </form>
         <div className={classes.postTime}>1 hour ago</div>
-      </Card>
-    </Fragment>
+      </div>
+    </Card>
   );
 };
 
