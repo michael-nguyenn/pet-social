2ba617a5ef22cc837bdc8d61198a8fde56e7e3bb
Michael Nguyen <n93michael@gmail.com>
2022-09-09 15:12:58 -0400

    env changes

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    DB_URL:
-      'mongodb+srv://mike:mikemike@pet-social.5anhhev.mongodb.net/?retryWrites=true&w=majority',
-  },
 };
 
 module.exports = nextConfig;
